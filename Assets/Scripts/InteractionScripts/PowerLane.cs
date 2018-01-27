72dd78e55208e17d0ec73ffbb8bf025e06dd4e44
Matthis Hagen <matthis.hagen@o2online.de>
2018-01-27 20:52:47 +0100

    - added 2 more level design elements

--- Assets/Scripts/InteractionScripts/PowerLane.cs
+++ Assets/Scripts/InteractionScripts/PowerLane.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerCharger : PlayerActionPoint {
+public class PowerLane : PlayerActionPoint {
 
-    public float maxPower;
-    bool _isActive;
+    public float powerGain;
 
     public override void Update()
     {
@@ -14,25 +13,25 @@ public class PowerCharger : PlayerActionPoint {
 
     public override void Start()
     {
-        _isActive = false;
+       
     }
 
     public override bool CheckPlayerAction(PlayerController playerController)
     {
-        // TODO: change to absorb!!!
-        return playerController.handleInput.IsPowerReleased();
+        // TODO: refactor this
+        return playerController.handleInput.IsAbsorbPressed();
     }
 
     public override bool isActive()
     {
 
-        return _isActive;
+        return false;
     }
 
+    // TODO: Refactor this stuff!
     public override void EffectGamePlay(PlayerController playerController, float reactionRating)
     {
         Debug.Log("Charging now!");
-        playerController.PowerGain(maxPower * reactionRating);
-        _isActive = true;
+        playerController.PowerGain(powerGain);
     }
 }
---
+++ Assets/Scripts/InteractionScripts/WallSegment.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSegment : PlayerActionPoint {
+
+    int walldirection;
+
+    public override void Update()
+    {
+
+    }
+
+    public override void Start()
+    {
+        if (transform.position.x > 0)
+            walldirection = 1;
+        else if (transform.position.x < 0)
+            walldirection = -1;
+        else
+        {
+            walldirection = 0;
+            Debug.Log("Do not place a wall in the centerLane!");
+        }
+
+        Transform cube = transform.GetComponentInChildren<MeshFilter>().transform;
+        cube.rotation = Quaternion.AngleAxis(90 * walldirection, Vector3.forward);
+
+        cube.position += new Vector3(walldirection * 0.5f, 0, cube.position.x);
+    }
+
+    public override bool CheckPlayerAction(PlayerController playerController)
+    {
+        if (walldirection == 1)
+            return playerController.handleInput.IsRightSliding() == false;
+        if (walldirection == -1)
+            return playerController.handleInput.IsLeftSliding() == false;
+        return true;
+    }
+
+    public override bool isActive()
+    {
+        return false;
+    }
+
+    // TODO: Refactor this stuff!
+    public override void EffectGamePlay(PlayerController playerController, float reactionRating)
+    {
+        playerController.EndGame();
+    }
+}
