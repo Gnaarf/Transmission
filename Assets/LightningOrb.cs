30dcc4708cff35c9e8cbf7a173e5528abfc0d4fc
kbornema <kai.bornemann@st.ovgu.de>
2018-01-27 14:03:57 +0100

    - visual effects - added absorb to joystick

---
+++ Assets/LightningOrb.cs
@@ -0,0 +1,12 @@
+﻿using DigitalRuby.LightningBolt;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningOrb : MonoBehaviour
+{
+
+    [SerializeField]
+    private LightningBoltScript _lightningBoltPrefab;
+
+}
